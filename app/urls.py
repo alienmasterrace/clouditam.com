fa59c8ed538205159aa989db067bf8980c9ada79
Dogukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-06-26 00:29:51 +0300

    api url conf and allauth conf added

--- app/urls.py
+++ app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
@@ -22,5 +22,13 @@ from app import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('web.urls')),
+    url(r'^api/v1/', include('api.urls')),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
