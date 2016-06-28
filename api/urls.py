8bd43d41e2b9fc9602d4799d1b28af538f454d5d
Doğukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-06-28 17:31:47 +0300

    initial rest framework examples

--- api/urls.py
+++ api/urls.py
@@ -1,26 +1,10 @@
 from django.conf.urls import include, url
-
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+from rest_framework import routers
+from api.views import UserCreateView, UserLoginView
 
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^accounts/create/$', UserCreateView.as_view()),
+    url(r'^accounts/login/$', UserLoginView.as_view()),
 ]
