796e07ab07484fa1b2f74812b91b593e89729d22
Doğukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-06-28 17:29:49 +0300

    initial views added

--- web/views.py
+++ web/views.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import View
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+
+class IndexView(View):
+    template_name = 'web/index.html'
+
+    # @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class DashboardView(View):
+    template_name = 'web/dashboard.html'
+
+    # @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class AccountView(View):
+    template_name = 'web/account/profile.html'
+
+    # @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class LoginView(View):
+    template_name = 'web/index.html'
+
+    # @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class RegisterView(View):
+    template_name = 'web/index.html'
+
+    # @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
