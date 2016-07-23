8a2004f67b0c2eff997630da093384abe9d90fce
Dogukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-07-23 23:22:50 +0300

    dashboard initlized models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    pass
+
+
+class Company(models.Model):
+    pass
+
+
+class Location(models.Model):
+    pass
+
+
+class Supplier(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Application(models.Model):
+    pass
+
+
+class Hardware(models.Model):
+    pass
+
+
+class Asset(models.Model):
+    pass
+
+
+class Status(models.Model):
+    pass
+
+
+class DashUser(models.Model):
+    pass
