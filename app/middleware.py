ea83585c72a9347f176a9973d227f005566d6801
Dogukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-10-05 16:42:05 +0300

    server admin port changed

--- app/middleware.py
+++ app/middleware.py
@@ -6,5 +6,5 @@ from app.settings import DEVELOPMENT
 class ProtectAdminMiddleware(object):
     def process_request(self, request):
         if not DEVELOPMENT:
-            if request.path.startswith("/admin") and request.META["SERVER_PORT"] != "8888":
+            if request.path.startswith("/admin") and request.META["SERVER_PORT"] != "9678":
                 raise Http404
