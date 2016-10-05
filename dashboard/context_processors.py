36a5db6265071578388757ab3aee8d37808a23e5
Dogukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-10-06 00:50:37 +0300

    fix no customer

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,3 +1,4 @@
 def bar(request):
-    return {"bar": 100 * (len(request.user.customer.assets.all()) / request.user.customer.asset_limit)}
+    if request.user.customer:
+        return {"bar": 100 * (len(request.user.customer.assets.all()) / request.user.customer.asset_limit)}
 
